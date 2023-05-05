cc0ae2b8faadfbbeab42e8c47d8740997d941baf
droneipcb <71715301+droneipcb@users.noreply.github.com>
2023-05-05 14:14:24 +0100

    Delete index.js

--- timer 9/index.js
+++
@@ -1,9 +0,0 @@
-module.exports = async function (context, myTimer) {
-    var timeStamp = new Date().toISOString();
-    
-    if (myTimer.isPastDue)
-    {
-        context.log('JavaScript esta lento!');
-    }
-    context.log('Funciona!', timeStamp);   
-};
